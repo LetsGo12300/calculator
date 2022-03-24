571df653a7131a1ec41e967be672728b8a715ce1
Kat <katkatvdd@yahoo.com.ph>
2022-03-24 16:48:57 +0800

    Modified code for calculator computations

--- static/script.js
+++ static/script.js
@@ -1,46 +1,72 @@
 function add(num1, num2){
-    return num1 + num2;
+    return (num1 + num2).toString();
 }
 
 function subtract(num1, num2){
-    return num1 - num2;
+    return (num1 - num2).toString();
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return (num1 * num2).toString();
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    //rounding off to 4 decimal places
+    let ans = (num1/num2).toString();
+    if (ans.length > 5)
+        return (num1 / num2).toFixed(4);
+    else return ans;
 }
 
-function operate(operator, inputString){
-    let [a, b] = separateString(inputString);
+function operate(str){
+    if (str.indexOf('+') !== -1){
+        let [a,b] = splitString('+', str);
+        return add(a,b);
+    }
+    else if (str.indexOf('—') !== -1){
+        let [a,b] = splitString('—', str);
+        return subtract(a,b);
+    }
+    else if (str.indexOf('*') !== -1){
+        let [a,b] = splitString('*', str);
+        return multiply(a,b);
+    }
+    else if (str.indexOf('/') !== -1){
+        let [a,b] = splitString('/', str);
+        return divide(a,b);
+    }
+}
+
+function splitString(operator, str){
+    let [a, b] = str.split(operator);
     a = parseFloat(a);
     b = parseFloat(b);
-
-    if (operator === '+'){
-        return add(a, b);
-    }
-    else if (operator === '—'){
-        return subtract(a, b);
-    }
-    else if (operator === '*'){
-        return multiply(a, b);
-    }
-    else if (operator === '/'){
-        return divide(a, b);
-    }
-    else return 'SYNTAX ERROR';
+    
+    return [a, b];
 }
 
-function separateString(inputString){
-    return inputString.split('+').join(',').split('—').join(',').split('*').join(',').split('/').join(',').split(',');
+function containsOperatorLast(str) {
+    const operators = /[\/+—*]/;
+    return operators.test(str.slice(-1));
 }
 
-let result = '';
+function separateString(str){
+    return str.split('+').join(',').split('—').join(',').split('*').join(',').split('/').join(',').split(',');
+}
+
+function containsOperatorMid(str) {
+    let count = separateString(str).length;
+    const operators = /[\/+—*]/;
+    return (count > 1);
+}
+
+let topResult = '';
+let bottomResult = '';
+let lastClicked = null; 
 let operateFlag = false;
-let outputDisplay = document.getElementById('result');
+let equalsFlag = false;
+let topDisplay = document.getElementById('result');
+let bottomDisplay = document.getElementById('input');
 
 let buttons = document.querySelectorAll(".calc-btn");
 
@@ -50,31 +76,72 @@ buttons.forEach(btn => {
 
         if (btnClass.includes("action-btn")){
             if (btn.id === 'clear') {
-                result = '';
+                topResult = ''; 
+                bottomResult = '';
+                topDisplay.textContent = topResult;
+                bottomDisplay.textContent = bottomResult;
             }
             else if (btn.id === 'sign-reversal') {
-                result = '-' + result;
+                if (topResult.substring(0,1) === '-'){
+                    topResult = topResult.slice(1);
+                }
+                else topResult = '-' + topResult;
             }
             else if (btn.id === 'del') {
-                result = result.slice(0,-1);
+                topResult = topResult.slice(0,-1);
+                bottomResult = bottomResult.slice(0,-1);
             }
         }
         else if (btnClass.includes("operand")){
-            result += btn.value;
+            if (containsOperatorLast(topResult)){
+                bottomResult = '';
+            }
+            bottomResult += btn.value;
+            topResult += btn.value;
         }
         else if (btnClass.includes("operator")){
-            clickedOperator = btn.value;
-            result += btn.value;
+            
+            if (containsOperatorLast(topResult)){
+                topResult = topResult.slice(0,-1) + btn.value;
+            }
+            else if (containsOperatorMid(topResult)){
+                equalsFlag = true;
+                operateFlag = true;
+            }
+            else {
+                topResult += btn.value;
+            }
         }
         else if (btnClass.includes("equals")){
-            operateFlag = true;
+            if (topResult !== '')
+                equalsFlag = true;
         }
-        
-        if (operateFlag === true){  
-            result = operate(clickedOperator, result);
+
+        if (lastClicked === 'equals-btn'){
+            //pass
+        }
+        else if (equalsFlag === true){
+            if (operateFlag === true){
+                topResult = operate(topResult);
+                topResult += btn.value;
+                topDisplay.textContent = topResult;
+                bottomDisplay.textContent = topResult.slice(0,-1);
+            } 
+            else {
+                topResult = operate(topResult);
+                topDisplay.innerHTML += '=';
+                bottomDisplay.textContent = topResult;
+            }
+            bottomResult = '';
+            equalsFlag = false;
             operateFlag = false;
         }
-        
-        outputDisplay.textContent = result.toString();
+        else {
+            topResult = topResult.toString();
+            topDisplay.textContent = topResult;
+            bottomDisplay.textContent = bottomResult;
+        }
+
+        lastClicked = btn.id;
     });
 });
